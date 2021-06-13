234d5f4f7adbb73edeb5d2303f81591375569537
silvnst <silvan.stoeckli@intergga,ch>
2021-06-13 16:46:59 +0200

    Debug false try 1

--- commerce/settings.py
+++ commerce/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
-import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q')
 
 # SECURITY WARNING: don't run with debug turned on in production!
